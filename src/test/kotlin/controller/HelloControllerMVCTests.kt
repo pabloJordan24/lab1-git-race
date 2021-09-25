e3b615d15b2508ff8202e8491300167b6d6e96aa
fjlopez <fjlopez@unizar.es>
2021-09-25 15:33:23 +0200

    Fixed comments

--- src/test/kotlin/controller/HelloControllerMVCTests.kt
+++ src/test/kotlin/controller/HelloControllerMVCTests.kt
@@ -1,40 +1,38 @@
 package es.unizar.webeng.hello.controller
 
-//import libraries (junit used for testing, spring for web app).
 import org.hamcrest.CoreMatchers.*
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-//import also mocking libraries used to create useful mocks for testing
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
-//Annotation used to apply only configuration relevant to MVC test.
+/**
+ * **Note:**
+ * [WebMvcTest] is an annotation used to apply only configuration relevant to MVC test.
+ */
 @WebMvcTest(HelloController::class)
-
 class HelloControllerMVCTests {
     /**
      * Assigns the property `app.message` in `application.properties` to [message]
+     * 
+     * **Note:**
+     * We use [Value] to inicialize the properties.
+     * We take advantage of Spring´s expression language to access the values using `${value}`.
      */ 
-
-    //We use this annotation to inicialize the properties.
-    //We take advantage of Spring´s expression language to access the values
-    //using ${value}.
-
-    //A variable (message) of type String is initalized to be able to store the values of the test.
-    //we want to prove
     @Value("\${app.message}") private lateinit var message: String
 
     /**
-     * Mocks the Spring controller
+     * Mocks the Spring controller.file
+     * 
+     * **Note:**
+     * Spring can enable dependency injection in an automatic mode. 
+     * This can be done thanks to this [Autowired] annotation.
+     * Great option if you want to mantain thecode because it updates automatically.
      */
-
-    //Spring can enable dependency injection in an automatic mode. This can be 
-    //done thanks to this AutoWired annotation. (Great option if you want to mantain the
-    //code because it updates automatically).
     @Autowired private lateinit var mockMvc: MockMvc
 
     /**
@@ -44,21 +42,21 @@ class HelloControllerMVCTests {
      * - print the response
      * - expect to receive an OK status (code 200)
      * - expect the atributte "message" of the model to be [message]
+     * 
+     * **Note:**
+     * Annotation [Test] is used to specify the body of a unit test.
      */
-
-     //Annotation @Test is used to specify the body of a unit test.
-
     @Test
     fun testMessage() {
-        //Method .perfom executes a get operation to the URL specified. In this 
-        //case, to the root dir "/".
+        // Method .perfom executes a get operation to the URL specified. 
+        // In this case, to the root dir "/".
         mockMvc.perform(get("/"))
-            //executes the print method (using the mock)
+            // Executes the print method (using the mock)
             .andDo(print())
-            //expects that no error occurred (isOk proves it)
+            // Expects that no error occurred (isOk proves it)
             .andExpect(status().isOk)
-            //and also expects that the attribute message is equal to the one 
-            //previously initialized (the app.message value)
+            // And also expects that the attribute message is equal to the one 
+            // previously initialized (the app.message value)
             .andExpect(model().attribute("message", equalTo(message)))
     }
 }
